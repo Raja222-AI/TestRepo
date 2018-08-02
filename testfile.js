82caa27b09a895b73e903b8c32ad49df4ca725ea
Muralidharan Selvaraj <muralidharan.selvaraj@oracle.com>
2018-08-02 13:27:22 +0530

    local changes

--- testfile.js
+++ testfile.js
@@ -2,5 +2,10 @@
 //v 1.0
 
 function getVersion() {
-  console.log("Version 1.0 master")
+  console.log("Version 1.0 master");
+  console.log("Added in local");
+}
+
+function localRepoChange() {
+  console.log("This is added in local repo");
 }
